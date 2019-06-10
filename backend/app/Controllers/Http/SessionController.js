ec7d23f20190910130128c573de1ef2f06defa3b
Petrovick Ubuntu <petrovickg@hotmail.com>
2019-06-10 17:44:53 -0300

    cors

--- backend/app/Controllers/Http/SessionController.js
+++ backend/app/Controllers/Http/SessionController.js
@@ -2,10 +2,12 @@
 
 class SessionController {
   async store ({ request, auth }) {
+    console.log('Entrou no Sessions')
     const { email, password } = request.all()
     const token = await auth.attempt(email, password)
     return token
   }
+  
 }
 
 module.exports = SessionController
